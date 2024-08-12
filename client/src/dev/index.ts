53a28ea1fb20f25e61fc51c69a957465b0af8b7a
John Wakley <john.wakley@icloud.com>
2024-08-12 07:44:09 -0400

    started dev section

---
+++ client/src/dev/index.ts
@@ -0,0 +1,4 @@
+import '../css/dev.css';
+import { DEV_LOGGER as log } from '../utils/';
+
+log('main.ts:', import.meta.env.DEV, typeof import.meta.env.DEV);
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,2 @@
 import './css';
-import './css/dev.css';
-import { DEV_LOGGER as log } from './utils/';
-
-log('main.ts:', import.meta.env.DEV, typeof import.meta.env.DEV);
+import './dev';
