7689f4596873696455c7aec0f75243735c8eeb93
John Wakley <john.wakley@icloud.com>
2024-08-13 16:25:38 -0400

    updated package-lock, dev-logger

--- client/src/utils/dev-logger.ts
+++ client/src/utils/dev-logger.ts
@@ -1,5 +1,9 @@
 import { DEV } from './env';
 
 export const DEV_LOGGER = DEV
-    ? console.debug.bind(console, '[dev]')
+    ? console.debug.bind(
+        console,
+        '%c[dev]',
+        'background: cyan; color: black; padding: 2px; border-radius: 4px; font-weight: bold;',
+    )
     : (..._: any[]): void => { };
